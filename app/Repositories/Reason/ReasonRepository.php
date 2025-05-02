e79cb0c127882bbf52600e86b0af28acfce1887f
Farrel Akbar <farrelakbar432@gmail.com>
2025-05-02 10:54:14 +0700

    feat:create repository for table reason

---
+++ app/Repositories/Reason/ReasonRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Repositories\Reason;
+use App\Models\ReasonModel;
+use Illuminate\Database\Eloquent\Collection;
+
+class ReasonRepository implements ReasonRepositoryInterface{
+    
+    protected $model;
+
+    public function __construct(ReasonModel $model)
+    {
+        $this->model = $model;
+    }
+
+    public function all(): Collection
+    {
+        return $this->model->get();
+    }
+
+    public function find($id)
+    {
+        return $this->model->findOrFail($id);
+    }
+
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        $homepage = $this->model->findOrFail($id);
+        $homepage->update($data);
+        return $homepage;
+    }
+
+    public function delete($id)
+    {
+        return $this->model->findOrFail($id)->delete();
+    }
+}
