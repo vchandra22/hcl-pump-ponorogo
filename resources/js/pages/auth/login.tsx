ae2a25bf70eb9b62c00c90ea24be201dce87dbd4
vchandra22 <vincentchaandra@gmail.com>
2025-05-05 22:41:29 +0700

    feat: hapus fitur register

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -36,13 +36,13 @@ export default function Login({ status, canResetPassword }: LoginProps) {
     };
 
     return (
-        <AuthLayout title="Log in to your account" description="Enter your email and password below to log in">
+        <AuthLayout title="Masuk ke Akun Anda" description="Masukkan email dan kata sandi Anda di bawah ini untuk masuk.">
             <Head title="Log in" />
 
             <form className="flex flex-col gap-6" onSubmit={submit}>
                 <div className="grid gap-6">
                     <div className="grid gap-2">
-                        <Label htmlFor="email">Email address</Label>
+                        <Label htmlFor="email">Alamat Email</Label>
                         <Input
                             id="email"
                             type="email"
@@ -62,7 +62,7 @@ export default function Login({ status, canResetPassword }: LoginProps) {
                             <Label htmlFor="password">Password</Label>
                             {canResetPassword && (
                                 <TextLink href={route('password.request')} className="ml-auto text-sm" tabIndex={5}>
-                                    Forgot password?
+                                    Lupa Password?
                                 </TextLink>
                             )}
                         </div>
@@ -87,21 +87,21 @@ export default function Login({ status, canResetPassword }: LoginProps) {
                             onClick={() => setData('remember', !data.remember)}
                             tabIndex={3}
                         />
-                        <Label htmlFor="remember">Remember me</Label>
+                        <Label htmlFor="remember">Ingat saya</Label>
                     </div>
 
                     <Button type="submit" className="mt-4 w-full" tabIndex={4} disabled={processing}>
                         {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
-                        Log in
+                        Masuk
                     </Button>
                 </div>
 
-                <div className="text-muted-foreground text-center text-sm">
-                    Don't have an account?{' '}
-                    <TextLink href={route('register')} tabIndex={5}>
-                        Sign up
-                    </TextLink>
-                </div>
+                {/*<div className="text-muted-foreground text-center text-sm">*/}
+                {/*    Don't have an account?{' '}*/}
+                {/*    <TextLink href={route('register')} tabIndex={5}>*/}
+                {/*        Sign up*/}
+                {/*    </TextLink>*/}
+                {/*</div>*/}
             </form>
 
             {status && <div className="mb-4 text-center text-sm font-medium text-green-600">{status}</div>}
